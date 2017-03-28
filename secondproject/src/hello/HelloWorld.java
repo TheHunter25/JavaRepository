71be8defc39e1b8ea15aed0f0a4cb0e386ecd9c9
TheHunter25 <hg179680@students.davenport.k12.ia.us>
2017-03-28 13:21:35 -0500

    update

---
+++ secondproject/src/hello/HelloWorld.java
@@ -0,0 +1,31 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+System.out.println("Look Mom Im programming, by the way Mr.brosius is not A very good speller");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+
+}
