5176b1b0613f8397d118446245c40cb84107eb40
TheHunter25 <hg179680@students.davenport.k12.ia.us>
2017-03-28 13:36:14 -0500

    update

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/cb/00683d69e513001716a5aa6eb2e379d8
@@ -0,0 +1,19 @@
+package Caseproblems;
+
+public class CarlysMotto {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("********************************");
+		System.out.println("********************************");
+		System.out.println("********************************");
+		System.out.println("********************************");
+		System.out.println("********************************");
+		System.out.println("********************************");
+	}
+
+
+	
+	
+}
--- Chapter1/src/Caseproblems/CarlysMotto.java
+++ Chapter1/src/Caseproblems/CarlysMotto.java
@@ -5,12 +5,10 @@ public class CarlysMotto {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("********************************");
-		System.out.println("********************************");
-		System.out.println("********************************");
-		System.out.println("********************************");
-		System.out.println("********************************");
-		System.out.println("********************************");
+		System.out.println("***********************************************");
+		System.out.println("*Carly's makes the food that makes it a party!*");
+		System.out.println("*      Sammy's makes it fun in the sun.       *");
+		System.out.println("***********************************************");
 	}
 
 
